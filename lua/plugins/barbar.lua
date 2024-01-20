0c5a84b64439d6abc1b03e14d4b090ed61e8ade7
xOnlinEx <daniel.floresvargas666@gmail.com>
2024-01-20 15:18:59 -0400

    add: shortcut for barbar

--- lua/plugins/barbar.lua
+++ lua/plugins/barbar.lua
@@ -4,4 +4,11 @@ return {
 		"lewis6991/gitsigns.nvim",
 		"nvim-tree/nvim-web-devicons",
 	},
+	config = function()
+		local map = vim.api.nvim_set_keymap
+		local opts = { noremap = true, silent = true }
+
+		-- Move to previous/next
+		map("n", "<Tab>", "<Cmd>BufferPrevious<CR>", opts)
+	end,
 }
