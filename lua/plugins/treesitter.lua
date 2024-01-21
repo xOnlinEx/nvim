5283eec06b7d048d950cd29136ca0392ba3ebe95
xOnlinEx <daniel.floresvargas666@gmail.com>
2024-01-20 21:27:22 -0400

    modify: lsp-config, global vim

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -5,7 +5,7 @@ return {
     config = function()
       local config = require("nvim-treesitter.configs")
       config.setup({
-        ensure_installed = {"lua"},
+        ensure_installed = {"lua","javascript"},
         highlight = { enable = true },
         indent = { enable = true },
       })
