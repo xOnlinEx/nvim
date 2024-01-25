101268345788113cb3cee1b5a6e2c79bc709e2f2
xOnlinEx <daniel.floresvargas666@gmail.com>
2024-01-24 23:36:33 -0400

    modify: theme everblush for github dark

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -7,7 +7,7 @@ return {
 		require("lualine").setup({
 			options = {
 				icons_enabled = true,
-				theme = "everblush",
+				theme = "github_dark",
 				component_separators = { left = "", right = "" },
 				section_separators = { left = "", right = "" },
 				disabled_filetypes = {
