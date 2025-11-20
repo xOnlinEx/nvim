330ee8b5b2166c2b47c4c938d29ffeef67d9a28d
xOnlinEx <daniel.floresvargas666@gmail.com>
2025-11-19 22:49:55 -0400

    feat(java): add Java and Spring Boot tooling to Mason config

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,18 +1,17 @@
 return {
-	-- JAVA + SPRING BOOT
-	{
-		import = "lazyvim.plugins.extras.lang.java",
-	},
+	{ import = "lazyvim.plugins.extras.lang.java" },
 	{
 		"mason-org/mason.nvim",
-		opts = {
-			ensure_installed = {
+		opts = function(_, opts)
+			opts.ensure_installed = opts.ensure_installed or {}
+
+			vim.list_extend(opts.ensure_installed, {
 				"jdtls",
 				"java-debug-adapter",
 				"java-test",
 				"google-java-format",
 				"vscode-spring-boot-tools",
-			},
-		},
+			})
+		end,
 	},
 }
