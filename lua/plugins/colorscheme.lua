53fd474261cf368068ac573967ea5d0012be0539
xOnlinEx <daniel.floresvargas666@gmail.com>
2025-11-24 22:28:23 -0400

    style: introduce stylua.toml to enforce Lua formatting style

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,74 +1,61 @@
 return {
-	{
-		"Gentleman-Programming/gentleman-kanagawa-blur",
-		name = "gentleman-kanagawa-blur",
-		priority = 1000,
-		opts = {},
-	},
-	{
-		"projekt0n/github-nvim-theme",
-		name = "github-theme",
-		lazy = false,
-		priority = 1000,
-		config = function()
-			require("github-theme").setup({
-				options = {
-					transparent = false,
-				},
-			})
-		end,
-	},
-	{
-		"catppuccin/nvim",
-		name = "catppuccin",
-		priority = 1000,
-		opts = {
-			background = {
-				light = "latte",
-				dark = "mocha",
-			},
-			transparent_background = true,
-			float = {
-				transparent = true,
-				solid = false,
-			},
-		},
-	},
-	{
-		"vague-theme/vague.nvim",
-		lazy = false,
-		priority = 1000,
-	},
-	{
-		"ellisonleao/gruvbox.nvim",
-		priority = 1000,
-		opts = {
-			transparent_mode = true,
-		},
-	},
-	{
-		"webhooked/kanso.nvim",
-		lazy = false,
-		priority = 1000,
-		opts = {
-			background = { -- map the value of 'background' option to a theme
-				dark = "ink", -- try "zen", "mist" or "pearl" !
-				light = "pearl", -- try "zen", "mist" or "pearl" !
-			},
-		},
-	},
-	{
-		"Mofiqul/vscode.nvim",
-		opts = {
-			transparent = true,
-			italic_comments = true,
-		},
-	},
-	-- Configure LazyVim for colorscheme
-	{
-		"LazyVim/LazyVim",
-		opts = {
-			colorscheme = "kanso",
-		},
-	},
+  {
+    "Gentleman-Programming/gentleman-kanagawa-blur",
+    name = "gentleman-kanagawa-blur",
+    priority = 1000,
+    opts = {},
+  },
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    priority = 1000,
+    opts = {
+      background = {
+        light = "latte",
+        dark = "mocha",
+      },
+      transparent_background = true,
+      float = {
+        transparent = true,
+        solid = false,
+      },
+    },
+  },
+  {
+    "vague-theme/vague.nvim",
+    lazy = false,
+    priority = 1000,
+  },
+  {
+    "ellisonleao/gruvbox.nvim",
+    priority = 1000,
+    opts = {
+      transparent_mode = true,
+    },
+  },
+  {
+    "webhooked/kanso.nvim",
+    lazy = false,
+    priority = 1000,
+    opts = {
+      background = {
+        dark = "ink",
+        light = "pearl"
+      },
+    },
+  },
+  {
+    "Mofiqul/vscode.nvim",
+    opts = {
+      transparent = true,
+      italic_comments = true,
+    },
+  },
+  -- Configure LazyVim for colorscheme
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "kanso",
+    },
+  },
 }
--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -1,12 +1,12 @@
 return {
-	"mason-org/mason.nvim",
-	opts = {
-		ensure_installed = {
-			"google-java-format",
-			"java-debug-adapter",
-			"java-test",
-			"jdtls",
-			"vscode-spring-boot-tools",
-		},
-	},
+  "mason-org/mason.nvim",
+  opts = {
+    ensure_installed = {
+      "google-java-format",
+      "java-debug-adapter",
+      "java-test",
+      "jdtls",
+      "vscode-spring-boot-tools",
+    },
+  },
 }
