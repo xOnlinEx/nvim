659dba386ab711564ca71827cb65d31591612acc
xOnlinEx <daniel.floresvargas666@gmail.com>
2025-12-07 19:14:03 -0400

    remove lazyextras from lazyvim.json

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -1,20 +1,25 @@
 return {
-  "mason-org/mason.nvim",
-  opts = {
-    ensure_installed = {
-      -- Java
-      "google-java-format",
-      "java-debug-adapter",
-      "java-test",
-      "jdtls",
-      "vscode-spring-boot-tools",
+  { import = "lazyvim.plugins.extras.lang.json" },
+  { import = "lazyvim.plugins.extras.lang.java" },
+  { import = "lazyvim.plugins.extras.lang.markdown" },
+  {
+    "mason-org/mason.nvim",
+    opts = {
+      ensure_installed = {
+        -- Java
+        "google-java-format",
+        "java-debug-adapter",
+        "java-test",
+        "jdtls",
+        "vscode-spring-boot-tools",
 
-      -- XML
-      "lemminx",
-      "xmlformatter",
+        -- XML
+        "lemminx",
+        "xmlformatter",
 
-      -- Markdown
-      "markdownlint-cli2",
+        -- Markdown
+        "markdownlint-cli2",
+      },
     },
   },
 }
