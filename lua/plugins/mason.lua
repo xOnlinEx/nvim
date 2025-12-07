0cb73c9f149d89005f82e813cb07e00d45b3fc30
xOnlinEx <daniel.floresvargas666@gmail.com>
2025-12-07 19:05:55 -0400

    remove dashboard

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -2,11 +2,18 @@ return {
   "mason-org/mason.nvim",
   opts = {
     ensure_installed = {
+      -- Java
       "google-java-format",
       "java-debug-adapter",
       "java-test",
       "jdtls",
       "vscode-spring-boot-tools",
+
+      -- XML
+      "lemminx",
+      "xmlformatter",
+
+      -- Markdown
       "markdownlint-cli2",
     },
   },
