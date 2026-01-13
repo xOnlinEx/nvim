e9b3a3e0801ba08edfe31657148a5494afde820a
xOnlinEx <daniel.floresvargas666@gmail.com>
2026-01-13 13:57:00 -0400

    modified neo-tree

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -1,6 +1,11 @@
 return {
   "nvim-neo-tree/neo-tree.nvim",
   opts = {
+    -- filesystem = {
+    --   follow_current_file = {
+    --     enabled = false,
+    --   },
+    -- },
     window = {
       position = "float",
       popup = {
