1b50f0c0c7e3e2bf02ea7a8779de42f7f6adec69
xOnlinEx <daniel.floresvargas666@gmail.com>
2026-02-10 17:29:40 -0400

    delete dankcolors and python lazyvim

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -17,7 +17,7 @@ return {
       transparent_background = false,
       float = {
         transparent = false,
-        solid = false,
+        zsolid = false,
       },
     },
   },
@@ -26,13 +26,6 @@ return {
     lazy = false,
     priority = 1000,
     opts = {},
-  },
-  {
-    "ellisonleao/gruvbox.nvim",
-    priority = 1000,
-    opts = {
-      transparent_mode = false,
-    },
   },
   {
     "webhooked/kanso.nvim",
@@ -82,69 +75,19 @@ return {
     },
   },
   {
-    "AlexvZyl/nordic.nvim",
+    "folke/tokyonight.nvim",
     lazy = false,
     priority = 1000,
     opts = {
-      transparent = {
-        -- Enable transparent background.
-        bg = false,
-        -- Enable transparent background for floating windows.
-        float = false,
-      },
-    },
-  },
-  {
-    "savq/melange-nvim",
-  },
-  {
-    "nyoom-engineering/oxocarbon.nvim",
-  },
-  {
-    "sainnhe/gruvbox-material",
-    lazy = false,
-    priority = 1000,
-  },
-  {
-    "oskarnurm/koda.nvim",
-    lazy = false, -- make sure we load this during startup if it is your main colorscheme
-    priority = 1000, -- make sure to load this before all the other start plugins
-    opts = {
+      style = "night",
       transparent = false,
     },
-  },
-  {
-    "yonatan-perel/lake-dweller.nvim",
-    lazy = false,
-    priority = 1000,
-  },
-  -- Using Lazy
-  {
-    "navarasu/onedark.nvim",
-    priority = 1000, -- make sure to load this before all the other start plugins
-    opts = {
-      style = "warmer",
-    },
-  },
-  {
-    "zenbones-theme/zenbones.nvim",
-    -- Optionally install Lush. Allows for more configuration or extending the colorscheme
-    -- If you don't want to install lush, make sure to set g:zenbones_compat = 1
-    -- In Vim, compat mode is turned on as Lush only works in Neovim.
-    dependencies = "rktjmp/lush.nvim",
-    lazy = false,
-    priority = 1000,
-    -- you can set set configuration options here
-    -- config = function()
-    --     vim.g.zenbones_darken_comments = 45
-    --     vim.cmd.colorscheme('zenbones')
-    -- end
   },
   -- Configure LazyVim for colorscheme
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "gruvbox-material",
+      colorscheme = "tokyonight",
     },
   },
 }
