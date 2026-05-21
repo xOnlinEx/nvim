66ead5d6c86aa55adda3a75ad1ccce711613a054
xOnlinEx <daniel.floresvargas666@gmail.com>
2026-05-20 20:50:30 -0400

    add: preserve yank on paste

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,6 +1,7 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+-- vim.keymap.set("x", "p", [["_dP]])
 
 -- Tmux Navigation
 local nvim_tmux_nav = require("nvim-tmux-navigation")
